4244e9cff160bc874d7d26f485e02e00f19fe90a
Andrew Scott <amscott@openmailbox.org>
2016-02-24 19:56:24 +0000

    Adds roman_numeralize method to integer

---
+++ lib/roman_numerals.rb
@@ -0,0 +1,33 @@
+class Integer
+
+  def roman_numeralize
+    result = ""
+    number = self
+    roman_numeral_mapping.keys.each do |divisor|
+      quotient, modulus = number.divmod(divisor)
+      result << roman_numeral_mapping[divisor] * quotient
+      number = modulus
+    end
+    result
+  end
+
+  private
+
+  def roman_numeral_mapping
+    {
+      1000 => "M",
+      900 => "CM",
+      500 => "D",
+      400 => "CD",
+      100 => "C",
+      90 => "XC",
+      50 => "L",
+      40 => "XL",
+      10 => "X",
+      9 => "IX",
+      5 => "V",
+      4 => "IV",
+      1 => "I"
+    }.freeze
+  end
+end
---
+++ roman_numerals.rb
@@ -0,0 +1,1 @@
+require './lib/roman_numerals'
